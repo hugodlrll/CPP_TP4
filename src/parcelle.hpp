73cfab0e8ee900c74484f18a3a8dfc8906d441f6
hugodlrll <hugo.del49@gmail.com>
2024-11-20 14:25:50 +0100

    [Evol] : Ajout de l'affichage dans la classe Parcelle + Passage en classe abstraite
    Signed-off-by: hugodlrll <hugo.del49@gmail.com>

--- src/parcelle.hpp
+++ src/parcelle.hpp
@@ -1,9 +1,11 @@
 #include <string>
+#include <iostream>
 #include "polygone.hpp"
 
 #ifndef PARCELLE_HPP
 #define PARCELLE_HPP
 
+using namespace std;
 class Parcelle {
     public:
         Parcelle(int num, string prop, Polygone<int,float>forme) ;
@@ -16,7 +18,7 @@ class Parcelle {
         void setNumero(int n);
         void setProprietaire(std::string prop);
         void setForme(Polygone<int> forme);
-        void setType(string type);
+        virtual void setType(string type) const = 0;
     protected:
         std::string type ;
         int numero ;
@@ -25,5 +27,13 @@ class Parcelle {
         Polygone<int> forme;
         int pConstructible;
 } ;
+
+ostream& operator<<(ostream& os, Parcelle & const p) {
+    os << "Parcelle n° " << p.getNumero() << " :" << endl;
+    os << "\tType : " << p.getType() << endl;
+    os << "\tPolygone : " << p.getForme() <<endl;
+    os << "\tPropriétaire : " << p.getProprietaire() << endl;
+    os << "\tSurface : " << p.getSurface() << endl;
+}
 
 #endif
