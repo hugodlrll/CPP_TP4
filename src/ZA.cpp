08ee53967b123cc8f04ee89cdb9be84464b1461c
hugodlrll <hugo.del49@gmail.com>
2024-11-25 17:40:13 +0100

    [Evol] -Dev de la classe ZA -Suppression de l'héritage dans Constructible
    Signed-off-by: hugodlrll <hugo.del49@gmail.com>

--- src/ZA.cpp
+++ src/ZA.cpp
@@ -1,7 +1,25 @@
 #include "ZA.hpp"
 
-ZA::ZA(std::string culture, int sMax, int pMax) : typeCulture(culture), sConstructibleMax(sMax), pConstructibleMax(pMax) {}
+ZA::ZA(int num, std::string prop, Polygone<int> forme, std::string culture)
+    : ZN(num, prop, forme), typeCulture(culture) {}
+
+int ZA::surfaceConstructible() {
+    int s = static_cast<int>(0.1 * this->getSurface());
+    if(s > 200)
+        return 200 ;
+    else
+        return s ;
+}
 
 std::string ZA::getCulture() {
-    return this->typeCulture;
+    return this->typeCulture ;
+}
+
+void ZA::setType() {
+    this->type = "ZA" ;
+}
+
+ostream& operator<<(ostream& os, ZA& z) {
+    os << "Type culture : " << z.getCulture() << endl ;
+    return os ;
 }
