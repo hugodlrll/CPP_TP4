ae741a55808eeca5ee18424a5f0c33ce8ad83389
marrev <julien.depeyris@laposte.net>
2024-11-25 18:00:25 +0100

    Implémentation calculSurface()

--- src/parcelle.cpp
+++ src/parcelle.cpp
@@ -1,6 +1,7 @@
 #include "parcelle.hpp"
+#include<iostream>
 
-Parcelle::Parcelle(int num, string prop, Polygone<int> forme) : numero(num), proprietaire(prop), forme(forme) {}
+Parcelle::Parcelle(int num, string prop, Polygone<int> forme) : numero(num), proprietaire(prop), forme(forme), surface(calculSurface(forme)) {}
 
 Parcelle::Parcelle(const Parcelle & parc) : type(parc.type), numero(parc.numero), surface(parc.surface), proprietaire(parc.proprietaire), forme(parc.forme) {}       
 
@@ -35,6 +36,23 @@ void Parcelle::setProprietaire(string prop){
 
 void Parcelle::setForme(Polygone<int> forme){
     this->forme = forme ;
+    calculSurface(forme);
+}
+
+float Parcelle::calculSurface(Polygone<int> forme){
+    float result = 0;
+    vector<Point2D<int>>::iterator itNext;
+    for(typename vector<Point2D<int>>::iterator it = forme.getSommets().begin(); it != forme.getSommets().end(); it++)
+    {
+        if(std::next(it,1) != forme.getSommets().end()){
+            itNext = std::next(it,1);
+        } else {
+            itNext = forme.getSommets().begin();
+        }
+        result +=  it->getX()*itNext->getY() - itNext->getX()*it->getY();
+        std::cout << it->getX() << " * " << itNext->getY() << " - " << itNext->getX() << " * " << it->getY() << std::endl;
+    }
+    return (abs(0.5*result));
 }
 
 ostream& operator<<(ostream& os, Parcelle& p) {
